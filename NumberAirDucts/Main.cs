40dc24bcc4860bf9be3238e363804cb6604cf6be
Sergei Prutsakov <psn206@mail.ru>
2024-08-06 16:53:41 +0300

    Добавьте файлы проекта.

---
+++ NumberAirDucts/Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAirDucts
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            List<Element> familyInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Element>()
+                .ToList();
+
+
+            TaskDialog.Show("?", $"Количество воздуховодов= {familyInstances.Count.ToString()}") ;
+            return Result.Succeeded;
+        }
+    }
+}
